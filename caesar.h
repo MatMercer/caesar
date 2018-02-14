37535bee1bbd65c94868be4460bccd75d153abf0
MatMercer <mateusmercer@gmail.com>
2018-02-14 02:24:32 -0200

    Add ifdef guard

--- caesar.h
+++ caesar.h
@@ -1,3 +1,6 @@
+#ifndef CAESAR_H
+#define CAESAR_H
+
 #include <stdbool.h>
 
 bool isLowerCase(int c);
@@ -5,3 +8,5 @@ int caesar(int c, int k);
 int caesarDict(int c, int k, char* dict);
 char* cleanDict(char* dict);
 bool charInDict(int c, char* dict);
+
+#endif
